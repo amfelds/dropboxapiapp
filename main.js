44f368ab90eec40b72a697562b09ddc5b43120e1
Alex Feldman <alexandramfeldman@gmail.com>
2013-11-03 13:24:26 -0800

    Built new js file off of My Tasks example

--- main.js
+++ main.js
@@ -1,32 +1,107 @@
-window.onload = function () {
-		
-	// Copy/pasted to create a dropbox client 
-	// from <https://www.dropbox.com/developers/datastore/tutorial/js>
-	var client = new Dropbox.Client({key: "8np8mfweu7hax9b"});
+// Insert your Dropbox app key here:
+var DROPBOX_APP_KEY = '8np8mfweu7hax9b';
 
-	// Try to finish OAuth authorization.
-	client.authenticate({interactive: false}, function (error) {
-	    if (error) {
-	        alert('Authentication error: ' + error);
-	    }
-	});
+// Exposed for easy access in the browser console.
+var client = new Dropbox.Client({key: DROPBOX_APP_KEY});
+var recipeTable;
 
-	if (client.isAuthenticated()) {
-	    // Client is authenticated. Display UI.
-	    $("#notLoggedInUI").hide();
-	    $("#loggedInUI").show();
+$(function () {
+	// Insert a new task record into the table.
+	function insertRecipe(url, name, imgsrc, text) {
+		recipeTable.insert({
+			recipeName: name,
+			link: url,
+			created: new Date(),
+			imageurl: imgsrc,
+			text: text
+		});
 	}
-	// end copy/paste from dropbox developer tutorial
 	
-	var testAdd = function() {
-		alert("add recipe stub method to call!");
-	};
+	// updateList will be called every time the table changes.
+	function updateList() {
+		//$('#recipes').empty();
+
+		var records = recipeTable.query();
+
+		// Sort by creation time.
+		records.sort(function (recipeA, recipeB) {
+			if (recipeA.get('created') < recipeB.get('created')) return -1;
+			if (recipeA.get('created') > recipeB.get('created')) return 1;
+			return 0;
+		});
+
+		// Add an item to the list for each task.
+		for (var i = 0; i < records.length; i++) {
+			var record = records[i];
+			$('#recipes').append(
+				renderRecipe(record.getId(),
+					record.get('recipename'),
+					record.get('link'),
+					record.get('created'),
+					record.get('imageurl')));
+		}
+
+		addListeners();
+		$('#newRecipe').focus();
+	}
 	
-	document.getElementById("dropboxAuthLink").onclick = function () {
+	// The login button will start the authentication process.
+	$('#dropboxAuthLink').click(function (e) {
+		e.preventDefault();
+		// This will redirect the browser to OAuth login.
 		client.authenticate();
-	};
+	});
 	
-	document.getElementById("testAddRecipe").onclick = function () {
-		return testAdd();
-	};
-};
+	// Try to finish OAuth authorization.
+	client.authenticate({interactive:false}, function (error) {
+		if (error) {
+			alert('Authentication error: ' + error);
+		}
+	});
+	
+	if (client.isAuthenticated()) {
+		// Client is authenticated. Display UI.
+		$('#notLoggedInUI').hide();
+		$('#loggedInUI').show();
+
+		client.getDatastoreManager().openDefaultDatastore(function (error, datastore) {
+			if (error) {
+				alert('Error opening default datastore: ' + error);
+			}
+
+			recipeTable = datastore.getTable('recipes');
+
+			// Populate the initial task list.
+			updateList();
+
+			// Ensure that future changes update the list.
+			datastore.recordsChanged.addListener(updateList);
+		});
+	}
+	
+	// Render the HTML for a single task.
+	function renderRecipe(id, name, url, date, imgsrc) {
+		return $('<div>').attr('id', id).addClass('recipeCard');;
+	}
+	
+	// Register event listeners to handle completing and deleting.
+	function addListeners() {
+	}
+	
+	// Hook form submit and add the new task.
+	$('#testAddRecipe').submit(function (e) {
+		e.preventDefault();
+		if ($('#newRecipeName').val().length > 0) {
+			insertRecipe($('#newRecipeUrl').val(),
+						$('#newRecipeName').val(),
+						$('#newRecipeImgSrc').val(),
+						$('#newRecipeText').val());
+			$('#newRecipeUrl').val('');
+			$('#newRecipeName').val('');
+			$('#newRecipeImgSrc').val('');
+			$('#newRecipeText').val('');
+		}
+		return false;
+	});
+	
+});
