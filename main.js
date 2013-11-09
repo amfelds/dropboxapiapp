2f50d68bd448a94be0587cbf0397289982680fd0
Alex Feldman <alexandramfeldman@gmail.com>
2013-11-09 11:50:31 -0800

    Sorted recipes and grouped by month / year. css is wonky, but no need to fix that before putting into bootstrap.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// THIS IS THE FILE WHERE ALL THE DROPBOX API STUFF IS WORKING TO THE BEST OF MY ABILITIES
+
+
 // Insert your Dropbox app key here:
 var DROPBOX_APP_KEY = '8np8mfweu7hax9b';
 var HOME_URL = 'http://localhost:8000/dropbox/dropboxapiapp/home.html'
@@ -7,10 +10,21 @@ var client = new Dropbox.Client({key: DROPBOX_APP_KEY});
 var recipeTable;
 var queryParams;
 
+// TODO: make month string array
+var monthMap = {0: 'January',
+	1: 'February',
+	2: 'March',
+	3: 'April',
+	4: 'May',
+	5: 'June',
+	6: 'July',
+	7: 'August',
+	8: 'September',
+	9: 'October',
+	10: 'November',
+	11: 'December'}
 
 $(function () {
-	alert('main.js is run!');
-
 	// This function is from http://snipplr.com/view/19838/get-url-parameters/
 	function getUrlVars() {
 		var map = {};
@@ -43,19 +57,77 @@ $(function () {
 			if (recipeA.get('created') > recipeB.get('created')) return 1;
 			return 0;
 		});
+		
+		var monthBuckets = [];
+		var yearBuckets = [];
+		var currDate = new Date();
 
-		// Add an item to the list for each task.
-		for (var i = 0; i < records.length; i++) {
-			var record = records[i];
-			$('#recipeList').append(
-				renderListItemRecipe(record.getId(),
-					record.get('recipename'),
-					record.get('link'),
-					record.get('created'),
-					record.get('imageurl')
-				)
-			);
+		// Sort the recipes into log-scale reverse-chronological buckets
+		// (remember getMonth() >> 0 is January)
+		var c = 0;
+		while (c < records.length) {
+			var record = records[c];
+			// if it's this year, put it into a month bucket
+			if (record.get('created').getYear() === currDate.getYear()) {
+				// create a bucket 
+				var currbucket = [];
+				var currmonth = record.get('created').getMonth();
+				while (c < records.length && record.get('created').getMonth() === currmonth) {
+					currbucket.push(record);
+					c++;
+					record = records[c];
+				}
+				monthBuckets.push(currbucket);
+			}
+			// if it's a former year, put it into a year bucket
+			else {
+				var currbucket = [];
+				var curryear = records.get('created').getYear();
+				while (c < records.length && record.get('created').getYear() === curryear) {
+					currbucket.push(record);
+					c++;
+					record = records[c];
+				}
+				yearBuckets.push(currbucket);
+			}
 		}
+		
+		// Add an item to the list for each month/year bucket
+		for (var i = 0; i < monthBuckets.length; i++) {
+			var bucket = monthBuckets[i];
+			var monthNum = bucket[0].get('created').getMonth();
+			var monthString = monthMap[monthNum];
+			$('#recipeList').append(
+				renderMonthList(monthString)
+			);
+			for (var j = 0; j < bucket.length; j++) {
+				var record = bucket[j];
+				$('#recipeList').append(
+					renderListItemRecipe(record.getId(),
+						record.get('recipename'),
+						record.get('link'),
+						record.get('created'),
+						record.get('imageurl')
+					)
+				);
+			}
+		}
+		for (var i = 0; i < yearBuckets.length; i++) {
+			var bucket = yearBuckets[i];
+		}
+
+		// Add an item to the list for each recipe.
+// 		for (var i = 0; i < records.length; i++) {
+// 			var record = records[i];
+// 			$('#recipeList').append(
+// 				renderListItemRecipe(record.getId(),
+// 					record.get('recipename'),
+// 					record.get('link'),
+// 					record.get('created'),
+// 					record.get('imageurl')
+// 				)
+// 			);
+// 		}
 
 		addListeners();
 		$('#newRecipe').focus();
@@ -122,14 +194,28 @@ $(function () {
 		}
 	}
 	
+	// Delete the record with a given ID.
+	function deleteRecord(id) {
+		recipeTable.get(id).deleteRecord();
+		// TODO: maybe make a temporary div to undo the delete?
+	}
+	
+	// Render the HTML for a month group
+	function renderMonthList(title) {
+		return $('<h3>').html(title);
+	}
+	
 	// Render the HTML for a recipe list item
 	function renderListItemRecipe(id, name, url, date, imgsrc) {
 		return $('<div>').attr('id', id).append(
-				$('<img>').attr('src', imgsrc).addClass('recipeCardPic')
+			$('<div>').append(
+					$('<img>').attr('src', imgsrc).addClass('recipeCardPic')
+				).append(
+					$('<p>').html(name).addClass('recipeCardTitle')
+				).addClass('recipeCard')
 			).append(
-				$('<p>').html(name).addClass('recipeCardTitle')
-			)
-			.addClass('recipeCard');
+				$('<button>').addClass('delete').html('&times;')
+			);
 	}
 	
 	// Render the HTML for a selected recipe item
@@ -150,6 +236,12 @@ $(function () {
 			var div = $(this);
 			var id = div.attr('id');
 			window.location.href = (window.location.href).split('?')[0] + "?id=" + id;	
+		});
+		
+		$('button.delete').click(function (e) {
+			e.preventDefault();
+			var id = $(this).parents('div').attr('id');
+			deleteRecord(id);
 		});
 		
 		$('#homeHeader').click(function (e) {
